7c48b3b4aa98aedce5e666381f4bac48dd5060a5
steven <steven.wallimann@bibliosansfrontieres.org>
2018-06-14 11:25:19 +0200

    We need to download the script buildmycube as well

--- start_buildmycube.sh
+++ start_buildmycube.sh
@@ -5,10 +5,12 @@ apt-get -y install jq amqp-tools
 wget -O /tmp/get_amqp_output.sh http://gogs/outils-interne/temps_modernes_v1/raw/master/get_amqp_output.sh
 wget -O /tmp/read_from_amqp.sh http://gogs/outils-interne/temps_modernes_v1/raw/master/read_from_amqp_ideasbox.sh
 wget -O /tmp/send_to_log_queue.sh http://gogs/outils-interne/temps_modernes_v1/raw/master/send_to_log_queue.sh
+wget -O /tmp/buildMyCube.sh https://raw.githubusercontent.com/ideascube/ansiblecube/oneUpdateFile/buildMyCube.sh
 
 chmod +x /tmp/get_amqp_output.sh
 chmod +x /tmp/read_from_amqp.sh
 chmod +x /tmp/send_to_log_queue.sh
+chmod +x /tmp/buildMyCube.sh
 
 output=$(/tmp/read_from_amqp.sh)
 
@@ -22,7 +24,7 @@ macaddress=$( ip a s eth0 | awk ' /link\/ether/ { print $2 } ' )
 
 /tmp/send_to_log_queue.sh "$now - INFO - Starting deployment of $deviceName on $inet_addr / $macaddress"
 
-/var/lib/ansible/local/buildMyCube.sh -n "$deviceName" -t "$timeZone"
+/tmp/buildMyCube.sh -n "$deviceName" -t "$timeZone"
 
 now=$(TZ="Europe/Paris" date +"%F %T,%3N")
 /tmp/send_to_log_queue.sh "$now - INFO - Finished deployment of $deviceName on $inet_addr / $macaddress"
